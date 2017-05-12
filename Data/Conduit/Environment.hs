5560ab553ed380b184686f4dd4e0ed952ecd344a
Sven Mattsen <sven@sven.cc>
2017-05-12 18:23:49 +0200

    use lists

--- Data/Conduit/Environment.hs
+++ Data/Conduit/Environment.hs
@@ -8,9 +8,7 @@ import Control.Monad.IO.Class
 import Data.Conduit
 import qualified Data.Conduit.Binary as CB
 import qualified Data.Conduit.List as CL
-import qualified Data.Sequence as S
 import qualified Data.ByteString as BS
-import Data.Sequence (Seq, (|>), (<|))
 import Data.Monoid
 import Data.List
 import Data.Char
@@ -19,13 +17,13 @@ import Control.Exception
 environments :: forall a m. ({-Show a, -}MonadResource m, Monoid a) => (Int, Int) -> a -> (a -> Bool) -> Conduit a m a
 environments (before, after) join pred = assert (before >= 0 && after >= 0) $ go (initBuf before) []
   where
-  initBuf :: Int -> Seq a
-  initBuf n = S.replicate n mempty
-  addBuf :: Seq a -> a -> Seq a
-  addBuf seq a = S.drop 1 seq |> a
-  addOuts :: [(Int, Seq a)] -> a -> [(Int, Seq a)]
-  addOuts outs val = map (\(ctr, s) -> (ctr - 1, s |> join |> val)) outs
-  go :: Seq a -> [(Int, Seq a)] -> Conduit a m a
+  initBuf :: Int -> [a]
+  initBuf n = replicate n mempty
+  addBuf :: [a] -> a -> [a]
+  addBuf seq a = drop 1 seq ++ [a]
+  addOuts :: [(Int, [a])] -> a -> [(Int, [a])]
+  addOuts outs val = map (\(ctr, s) -> (ctr - 1, s ++ [join,  val])) outs
+  go :: [a] -> [(Int, [a])] -> Conduit a m a
   go buf outs = do
     let (finished, unfinished) = partition (\(ctr, _) -> ctr == 0) outs
     {-liftIO $ putStrLn $ "finished: " ++ show finished-}
@@ -34,13 +32,9 @@ environments (before, after) join pred = assert (before >= 0 && after >= 0) $ go
     case chunk of
       Nothing -> forM_ unfinished $ \(_, s) -> yield (fold s)
       Just val -> do
-        let outs' = (unfinished ++ [(after + 1, sIntersperse join buf) | pred val])
+        let outs' = (unfinished ++ [(after + 1, intersperse join buf) | pred val])
         {-liftIO (putStrLn $ "input: " ++ show val ++ " buf: " ++ show buf ++ " unfinished: " ++ show unfinished ++ " outs': " ++ show outs')-}
         go (addBuf buf val) (addOuts outs' val)
-
-sIntersperse :: a -> Seq a -> Seq a
-sIntersperse a sa | S.null sa = sa
-                  | otherwise = let res = foldr (\e s -> e <| a <| s) S.empty sa in S.take (length res - 1) res
 
 lineEnvironments :: MonadResource m => (Int, Int) -> (BS.ByteString -> Bool) -> Conduit BS.ByteString m BS.ByteString
 lineEnvironments env pred = CB.lines =$= environments env "\n" pred =$= CL.map (flip BS.snoc $ fromIntegral $ ord '\n')
